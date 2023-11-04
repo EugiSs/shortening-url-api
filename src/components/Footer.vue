a25fa7bef14e6712567efddc55566fcaa20c931d
Evgenia <90983142+EugiSs@users.noreply.github.com>
2023-11-04 22:26:37 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -100,7 +100,7 @@
 			Challenge by
 			<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"
 				>Frontend Mentor</a
-			>. Coded by <a href="https://github.com/EvgiSs">EvgiSs</a>.
+			>. Coded by <a href="https://github.com/EugiSs">EugiSs</a>.
 		</div>
 	</footer>
 </template>
